77098827a33b0749d8d15a5b5ae2f23e9caf74bc
K-98-test <kkj120126@naver.com>
2025-05-05 01:28:43 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// vite.config.js
 export default defineConfig({
+  root: '.', // 또는 'src'가 아니라면 프로젝트 루트로 설정
+  build: {
+    outDir: 'dist',
+  },
   plugins: [react()],
-});
+})
