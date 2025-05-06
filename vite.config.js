cc421a69567517e2fad4ada7cdc03464598fd7a4
K-98-test <kkj120126@naver.com>
2025-05-07 06:06:49 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// vite.config.js
 export default defineConfig({
-  root: '.', // 또는 'src'가 아니라면 프로젝트 루트로 설정
-  base:'./',
+  root: '.',         // 루트 폴더가 프로젝트 최상위
+  base: './',         // 상대 경로로 설정 (Vercel에서 흰 화면 방지)
   build: {
-    outDir: 'dist',
+    outDir: 'dist'    // 빌드 결과 경로 (Vercel이 이걸 서빙)
   },
-  plugins: [react()],
+  plugins: [react()]
 })
