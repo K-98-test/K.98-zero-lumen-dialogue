221e78ae54f3a1afdc5788ff699a0d481bcd4894
K-98-test <kkj120126@naver.com>
2025-05-08 22:05:59 +0900

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';  // ✅ 확장자까지 정확하게 작성
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
